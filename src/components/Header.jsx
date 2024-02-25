72b02638482a5a3c7210aed267d94a6993cec432
Nurul Mukhlisa <numulaa@gmail.com>
2024-02-25 10:48:11 +0800

    fixed buttons styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,15 @@ import shoplistIcon from "../assets/woman-shopping.svg";
 
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const location = window.location.href;
+
+  // const [showShoplist, setShowShoplist] = useState(true);
+  // console.log(location, showShoplist);
+  // useEffect(() => {
+  //   if (location.substring(location.length - 10) === "myshoplist") {
+  //     setShowShoplist(false);
+  //   }
+  // }, [location]);
   const handleSignOut = () => {
     signOut(auth).then(() => {
       window.location.reload();
@@ -33,12 +42,14 @@ const Header = () => {
         </NavLink>
         {isLoggedIn ? (
           <ul>
-            <NavLink to="/myshoplist" className="nav-link">
+            <NavLink
+              to="/myshoplist"
+              // onClick={() => setIsMyShoplist(true)}
+              className="nav-link myshoplist-button">
               <li>My shoplist</li>
             </NavLink>
-
-            <li className="signup-button nav-link" onClick={handleSignOut}>
-              Log Out
+            <li className="logout-button nav-link" onClick={handleSignOut}>
+              <i className="fa-solid fa-right-from-bracket"></i>
             </li>
           </ul>
         ) : (
--- src/pages/MyShoplist.jsx
+++ src/pages/MyShoplist.jsx
@@ -6,14 +6,20 @@ import { auth } from "../firebase";
 import { Link } from "react-router-dom";
 import { collection, onSnapshot, query, where } from "firebase/firestore";
 import { db } from "../firebase";
+import CreateShoplistModal from "../components/CreateShoplistModal";
+import ItemWrapper from "../components/ItemWrapper";
+
 const MyShoplist = () => {
   const [isSignup, setIsSignup] = useState(false);
   const [shoplistCollection, setShoplistCollection] = useState([]);
+  const [isCreateShoplist, setIsCreateShoplist] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   // const user = auth.currentUser;
   const [userName, setUserName] = useState("");
 
   useEffect(() => {
+    setIsLoading(true);
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setUserName(user.displayName);
@@ -29,15 +35,22 @@ const MyShoplist = () => {
           }));
           setShoplistCollection(shoplistCollectionArr);
         });
+        setIsLoading(false);
         return unsub;
       }
     });
   }, [auth]);
+
   return (
     <div className="on-boarding">
       <main className="onboarding-main-section">
         <h1>Hello, {userName}</h1>
         <p>Here's the list of all your shoplist</p>
+        {isLoading && (
+          <div className="loader-wrapper">
+            <div className="loader"></div>
+          </div>
+        )}
         <ul className="list-of-shoplist ">
           {shoplistCollection.map((shoplist) => (
             <li
@@ -50,6 +63,16 @@ const MyShoplist = () => {
             </li>
           ))}
         </ul>
+        <button
+          onClick={() => setIsCreateShoplist(true)}
+          className="create-new-shoplist-button">
+          Create a new shoplist
+        </button>
+        {isCreateShoplist && (
+          <CreateShoplistModal
+            handleCloseModal={() => setIsCreateShoplist(false)}
+          />
+        )}
       </main>
     </div>
   );
